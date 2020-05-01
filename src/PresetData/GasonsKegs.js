094f76b4f59757a0cb52a89cd77cbda70403e95d
Keturah Howard <keturah.dev@gmail.com>
2020-05-01 09:05:02 -0700

    create and pass test to return pre-existing state as default

--- src/PresetData/GasonsKegs.js
+++ src/PresetData/GasonsKegs.js
@@ -1,49 +1,55 @@
 import {v4} from "uuid"
 
-export const Kegs = [
-  {
+export const id1 = v4()
+export const id2 = v4()
+export const id3 = v4()
+export const id4 = v4()
+export const id5 = v4()
+
+export const Kegs = {
+  id1: {
     name: "Marry Me Belle",
     brand: "Gaston",
     price: "$11",
     alcoholContent: "12%",
     description: "My specially made Vodka cocktail to get Belle to losen up and just marry me like I know she should.",
     pints: 124,
-    id: v4()
+    id: id1
   },
-  {
+  id2:{
     name: "Antler Decor",
     brand: "BuckWiser",
     price: "$8",
     alcoholContent: "6%",
     description: "A quality light beer for my lightweight friends to sit back and enjoy my decor.",
     pints: 124,
-    id: v4()
+    id: id2
   },
-  {
+  id3:{
     name: "Egged Barge",
     brand: "Gaston",
     price: "$15",
     alcoholContent: "9%",
     description: "Spiked Eggnog to meet my 5 dozen egg standards.",
     pints: 124,
-    id: v4()
+    id: id3
   },
-  {
+  id4:{
     name: "Lady Lifts",
     brand: "BlueBarrel",
     price: "$6",
     alcoholContent: "5%",
     description: "A light beer for the ladies to sip on while I raise there tables.",
     pints: 124,
-    id: v4()
+    id: id4
   },
-  {
+  id5:{
     name: "Kill the Beast",
     brand: "MonsterMan",
     price: "$10",
     alcoholContent: "9%",
     description: "A pre pitchfork raising rum to get in the hero's spirit. Ride with me brothers!",
     pints: 124,
-    id: v4()
+    id: id5
   }
-]
+}
