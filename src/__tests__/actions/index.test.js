79e86c6b142217572e64c3f2c8ba29914033a3b7
Keturah Howard <keturah.dev@gmail.com>
2020-05-01 11:05:31 -0700

    write/fail test that chcks action creator for addKeg

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -1,7 +1,28 @@
 import * as actions from "./../../actions"
 
 describe("application's actions", () => {
-  it("should toggleForm should create TOGGLE_FORM action", () => {
+  const newKeg = {
+    name: "Covered in Hair",
+    brand: "MansLiquor",
+    price: "$11",
+    alcoholContent: "10%",
+    description: "Every inch! *rips off shirt*",
+    pints: 124,
+    id: 9
+  }
+  it("toggleForm should create TOGGLE_FORM action", () => {
     expect(actions.toggleForm()).toEqual({type: "TOGGLE_FORM"})
+  })
+  it("addKeg should create ADD_KEG action", () => {
+    expect(actions.addKeg(newKeg)).toEqual({
+      type: "ADD_KEG",
+      name: "Covered in Hair",
+      brand: "MansLiquor",
+      price: "$11",
+      alcoholContent: "10%",
+      description: "Every inch! *rips off shirt*",
+      pints: 124,
+      id: 9
+    })
   })
 })
--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ export const toggleForm = () => ({
   type: a.TOGGLE_FORM
 })
 
-// export const addKeg = (newKeg) => {
-//   const {name, brand, price, alcoholContent, description, pints, id} = newKeg
-//   return {type: a.ADD_KEG, name, band, price, alcoholContent, description, pints, id}
-// }
+export const addKeg = (newKeg) => {
+  const {name, brand, price, alcoholContent, description, pints, id} = newKeg
+  // return {type: a.ADD_KEG, name, brand, price, alcoholContent, description, pints, id}
+}
