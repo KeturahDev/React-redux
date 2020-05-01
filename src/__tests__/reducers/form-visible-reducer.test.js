184c2768e306ef8d29283b7faf7cef793e9b4d60
Keturah Howard <keturah.dev@gmail.com>
2020-05-01 08:40:02 -0700

    create/fail test for toggling formVisibleReducer

--- src/__tests__/reducers/form-visible-reducer.test.js
+++ src/__tests__/reducers/form-visible-reducer.test.js
@@ -1,7 +1,14 @@
 import formVisibleReducer from "./../../reducers/form-visible-reducer"
 
 describe("formVisibleReducer", () => {
+  let action;
   test("should return default state", () => {
     expect(formVisibleReducer(false, {type:null})).toEqual(false)
+  })
+  test("should toggle bool return", () => {
+    action = {
+      type: "TOGGLE_FORM"
+    }
+    expect(formVisibleReducer(false, action)).toEqual(true)
   })
 })
