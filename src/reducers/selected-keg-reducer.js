05db98a147e18f0ae8c7a7b03393d01c153d9986
Keturah Howard <keturah.dev@gmail.com>
2020-05-01 10:12:42 -0700

    write code to pass test- remove selected keg

--- src/reducers/selected-keg-reducer.js
+++ src/reducers/selected-keg-reducer.js
@@ -2,17 +2,11 @@ export default (state={}, action) => {
   const {name, brand, price, alcoholContent, description, pints, id} = action
   switch(action.type) {
     case "SELECT_KEG":
-      return {
-        [id]:{
-          name,
-          brand,
-          price,
-          alcoholContent,
-          description,
-          pints,
-          id
-        }
-      }
+      state = {[id]:{name,brand,price,alcoholContent,description,pints,id}} 
+      return state
+    case "REMOVE_KEG":
+      state = {}
+      return state
     default:
       return state
   }
