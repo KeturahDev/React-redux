998d9ec0b4d731b65528c99c3229475ce9afd8bf
Keturah Howard <keturah.dev@gmail.com>
2020-05-01 12:05:27 -0700

    involve redux as storage on index.js level

--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/App';
+import {Provider} from "react-redux"
+import {createStore} from "redux"
+import rootReducer from "./reducers"
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
+
+//coming back from lunch:
+//sort not working becuase database is now an object full of objects attach to keys.. how can I sort? should I do away with sorting? can you sort by key values of an OBJECT
+//replace instances of setState with getState... refer to bean proj?
+//I can do iiiiit
 
