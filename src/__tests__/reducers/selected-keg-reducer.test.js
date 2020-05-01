54465cd207884effd1e2064c110aed0307efdac4
Keturah Howard <keturah.dev@gmail.com>
2020-05-01 09:48:56 -0700

    create/fail test for selecting keg

--- src/__tests__/reducers/selected-keg-reducer.test.js
+++ src/__tests__/reducers/selected-keg-reducer.test.js
@@ -1,7 +1,41 @@
 import selectedKegReducer from "./../../reducers/selected-keg-reducer"
 
 describe("selectedKegReducer", () => {
+  let action;
+  const selectedKeg = {
+    name: "Egged Barge",
+    brand: "Gaston",
+    price: "$15",
+    alcoholContent: "9%",
+    description: "Spiked Eggnog to meet my 5 dozen egg standards.",
+    pints: 124,
+    id: 3
+  }
   test("should return default empty object", () => {
     expect(selectedKegReducer({}, {type:null})).toEqual({})
+  })
+  test("should add selected keg", () => {
+    const {name, brand, price, alcoholContent, description, pints, id} = selectedKeg
+    action = {
+      type: "SELECT_KEG",
+      name,
+      brand,
+      price,
+      alcoholContent,
+      description,
+      pints,
+      id
+    }
+    expect(selectedKegReducer({}, action)).toEqual({
+      3: {
+        name: "Egged Barge",
+        brand: "Gaston",
+        price: "$15",
+        alcoholContent: "9%",
+        description: "Spiked Eggnog to meet my 5 dozen egg standards.",
+        pints: 124,
+        id: 3
+      }
+    })
   })
 })
