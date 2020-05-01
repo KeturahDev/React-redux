d1b9aa0e62d9cc52d194c9338092491f5e531cf4
Keturah Howard <keturah.dev@gmail.com>
2020-05-01 11:56:38 -0700

    pass tests to ensure getState of rootReducer returned same defaults as actual reducers

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,8 +1,10 @@
-import {combineReducers} from "redux"
 import formVisibleReducer from "../../reducers/form-visible-reducer"
 import kegListReducer from "../../reducers/keg-list-reducer"
 import selectedKegReducer from "../../reducers/selected-keg-reducer"
 import rootReducer from "./../../reducers"
+import {createStore} from "redux"
+
+const store = createStore(rootReducer)
 
 describe("rootReducer", () => {
   test("should return same default state as original reducers", () => {
@@ -11,5 +13,14 @@ describe("rootReducer", () => {
       kegList: {},
       selectedKeg: {}
     })
+  })
+  test("should have same default state as original formVisibleReducer", () => {
+    expect(store.getState().formVisible).toEqual(formVisibleReducer(false, {type:null}))
+  })
+  test("should have same default state as original kegListReducer", () => {
+    expect(store.getState().kegList).toEqual(kegListReducer({}, {type:null}))
+  })
+  test("should have same default state as original selectedKegReducer", () => {
+    expect(store.getState().selectedKeg).toEqual(selectedKegReducer({}, {type:null}))
   })
 })
